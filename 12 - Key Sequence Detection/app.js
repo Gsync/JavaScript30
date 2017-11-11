212a346546b4da480e6279cbd439dc147c97aa2c
Khuram Niaz <knshaikh@gmail.com>
2017-11-11 12:05:41 -0700

    Key detection Day12

---
+++ 12 - Key Sequence Detection/app.js
@@ -0,0 +1,11 @@
+const pressed = [];
+const code = 'github';
+
+window.addEventListener('keyup', e => {
+    pressed.push(e.key);
+    pressed.splice(-code.length-1, pressed.length-code.length);
+    if (pressed.join('').includes(code)) {
+        cornify_add();
+    }
+    console.log(pressed);
+})
